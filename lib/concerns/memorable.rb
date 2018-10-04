18bf187bbcc15320b7af1ae0c4845d9756433511
Sharon Mun <sharonymun@gmail.com>
2018-10-04 12:31:13 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
-
-def reset_all
-  all.clear
-end
-def count
-  all.count
-end
-
-
+module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
+  end
+ 
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
+  end
 end
